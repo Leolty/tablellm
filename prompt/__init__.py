6d872695942eb18b2ff00e5429c59ea5498cb94c
Leolty <til040@ucsd.edu>
2023-11-25 23:50:18 -0800

    delete prompt

--- prompt/__init__.py
+++
@@ -1,5 +0,0 @@
-from .header_check import header_check_prompt
-from .sort import base_sort_prompt, smart_sort_prompt, choice_sort_prompt, \
-                  omit_statement, cot_sort_prompt, direct_sort_prompt
-from .cot import cot_prompt
-from .agent import agent_prefix, smart_agent_prefix
